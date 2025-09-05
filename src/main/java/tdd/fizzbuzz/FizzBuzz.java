a8ff19c15d3f0dc385912c0e146fb1b4268c4cc0
ethan <ethan.yang@oocl.com>
2025-09-05 11:01:32 +0800

    feat:add countOff1

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,7 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    public static String countOff(int order) {
+        return "1";
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,34 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_1_when_input_1() {
+        int order = 1;
+
+        String word = FizzBuzz.countOff(order);
+
+        assertEquals("1", word);
+    }
+
+    @Test
+    public void should_return_fizz_when_input_can_divided_by_3() {
+        int order = 3;
+
+        String word = FizzBuzz.countOff(order);
+
+        assertEquals("fizz", word);
+    }
+
+    @Test
+    public void should_return_buzz_when_input_can_divided_by_5() {
+        int order = 5;
+
+        String word = FizzBuzz.countOff(order);
+
+        assertEquals("buzz", word);
+    }
 }
